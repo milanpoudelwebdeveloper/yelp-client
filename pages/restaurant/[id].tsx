62595e823646e17d8a7546e72d7d67a346ec078f
Milan Poudel <milanwebdeveloper1@gmail.com>
2022-08-11 15:16:56 +0545

    eature/install-bootstrap-react-icons-and-set-up-pages

---
+++ pages/restaurant/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestaurentDetail = () => {
+  return (
+    <div>RestaurentDetail</div>
+  )
+}
+
+export default RestaurentDetail
---
+++ pages/restaurant/update/[id].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UpdateRestaurant = () => {
+  return (
+    <div>UpdateRestaurant</div>
+  )
+}
+
+export default UpdateRestaurant
